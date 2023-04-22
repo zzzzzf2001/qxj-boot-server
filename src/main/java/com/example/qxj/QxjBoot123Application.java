18353671e176ea77bad27b6ead430a0530d05cd4
zzzzzf2001 <zzzzzf011129@163.com>
2023-04-22 10:50:38 +0800

    'commit'

---
+++ src/main/java/com/example/qxj/QxjBoot123Application.java
@@ -0,0 +1,13 @@
+package com.example.qxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QxjBoot123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QxjBoot123Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qxj/QxjBoot123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qxj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QxjBoot123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
