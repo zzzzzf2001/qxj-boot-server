5b5011b2bd5420429b6b8da3ba02b0cb81d777e4
zzzzzf2001 <zzzzzf011129@163.com>
2023-05-13 13:39:47 +0800

    改BUG

--- src/main/java/com/qxj/qingxiaojiamaster/utils/JWTUtils.java
+++ src/main/java/com/qxj/qingxiaojiamaster/utils/JWTUtils.java
@@ -30,7 +30,7 @@ public class JWTUtils {
     public static String getToken(HashMap<String, String> map){
         //设置默认时间
         Calendar calendar= Calendar.getInstance();
-        calendar.add(Calendar.MINUTE,5);
+        calendar.add(Calendar.DATE,1);
         //创建JWT实例
         JWTCreator.Builder builder = JWT.create();
         //将Payload传入JWT实例中
